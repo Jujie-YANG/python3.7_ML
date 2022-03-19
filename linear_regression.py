f3e66870cabad1985b996704f82c6be0ee4a2d87
Jujie-YANG <Yangjujie3619@outlook.com>
2022-03-19 17:31:47 +0000

    finish linear regression

--- linear_regression.py
+++ linear_regression.py
@@ -18,6 +18,18 @@ predict = "G3"
 
 X = np.array(data.drop([predict], 1))  # y axis
 y = np.array(data[predict])
-print(y)
 
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size = 0.1)
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.25)
+
+linear = linear_model.LinearRegression()
+
+linear.fit(x_train, y_train)
+acc = linear.score(x_test, y_test)
+print("accuracy", acc)
+print('Coefficient: ', linear.coef_)
+print('Intercept: ', linear.intercept_)
+
+predictions = linear.predict(x_test)
+
+for x in range(len(predictions)):
+    print(predictions[x], x_test[x], y_test[x])
