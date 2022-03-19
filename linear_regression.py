a519c6f27533d2a68c46a761b10c1a20d76c3565
Jujie-YANG <Yangjujie3619@outlook.com>
2022-03-19 18:28:41 +0000

    Saving & Training Multiple Models

--- linear_regression.py
+++ linear_regression.py
@@ -4,9 +4,11 @@ import pandas as pd
 import numpy as np
 import sklearn
 from sklearn import linear_model
-import matplotlib.pyplot as pyplot
-import pickle
 from sklearn.utils import shuffle
+import matplotlib.pyplot as plt
+from matplotlib import style # grid style
+import pickle
+
 
 # Predict students' grades
 
@@ -16,20 +18,46 @@ data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
 
 predict = "G3"
 
+
+
+'''
+# TRAIN MODEL MULTIPLE TIMES FOR BEST SCORE
+best = 0
+for _ in range(20):
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.2)
+    linear = linear_model.LinearRegression()
+
+    linear.fit(x_train, y_train)
+    acc = linear.score(x_test, y_test)
+    print("Accuracy", acc)
+
+    if acc > best:
+        best = acc
+        with open("studentGrades.pickle", "wb") as f:  # opens the file in binary format for writing
+            pickle.dump(linear, f) # pickle.dump() save model into our  created pickle file(f)
+'''
+
+# Loading Our Model
+# Now we have save the best model and then we can use linear to predict grades like before
+pickle_in = open("studentgrades.pickle", "rb")
+linear = pickle.load(pickle_in)
+print('Coefficient: ', linear.coef_)
+print('Intercept: ', linear.intercept_)
+
 X = np.array(data.drop([predict], 1))  # y axis
 y = np.array(data[predict])
 
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.25)
-
-linear = linear_model.LinearRegression()
-
-linear.fit(x_train, y_train)
-acc = linear.score(x_test, y_test)
-print("accuracy", acc)
-print('Coefficient: ', linear.coef_)
-print('Intercept: ', linear.intercept_)
-
 predictions = linear.predict(x_test)
 
 for x in range(len(predictions)):
     print(predictions[x], x_test[x], y_test[x])
+
+
+# Drawing and plotting model
+plot = "failures" # Change this to G1, G2, studytime or absences to see other graphs
+plt.scatter(data[plot], data["G3"])
+plt.legend(plot, loc='best')
+plt.xlabel(plot)
+plt.ylabel("Final Grade")
+plt.show()
